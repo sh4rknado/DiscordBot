8658b045692417c7f9408b0f4f21fbb943b89e92
Bertieaux Jordan <jordan.bertieaux@std.heh.be>
2021-01-31 13:17:34 +0100

    add SharkyBot Client

---
+++ SharkyBot.py
@@ -0,0 +1,27 @@
+
+from SharkyBot.ASharkyBot import ASharkyBot
+from discord.ext import timers
+import discord
+
+
+class SharkyBot:
+    def __init__(self):
+        intents = discord.Intents.all()
+        self.bot = ASharkyBot(command_prefix='$', intents=intents)
+        self.client = discord.Client()
+        self.bot.timer_manager = timers.TimerManager(self.bot)
+
+        # ////////////////////////// < EVENT FUNCTION > /////////////////////////////////////////
+
+        @self.bot.event
+        async def on_ready():
+            await self.bot.change_presence(activity=discord.Game('Tide of War'))
+            print("Bot Ready !")
+
+    def running_bot(self, token):
+        self.bot.run(token)
+
+
+if __name__ == '__main__':
+    bot = SharkyBot()
+    bot.running_bot('ODAxODkyNDU4ODMyMzMwNzcy.YAnSjA.PFnIRI4KVIPXbb66_Wl_YhwYMrA')
