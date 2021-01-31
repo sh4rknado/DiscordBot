68e861293f72218c1744b6cbd9cdc9cd769e65f4
Bertieaux Jordan <jordan.bertieaux@std.heh.be>
2021-01-31 13:33:38 +0100

    add SharkBot Event Command function

--- SharkyBot.py
+++ SharkyBot.py
@@ -2,6 +2,8 @@
 from SharkyBot.ASharkyBot import ASharkyBot
 from discord.ext import timers
 import discord
+from datetime import datetime
+import asyncio
 
 
 class SharkyBot:
@@ -18,10 +20,262 @@ class SharkyBot:
             await self.bot.change_presence(activity=discord.Game('Tide of War'))
             print("Bot Ready !")
 
-    def running_bot(self, token):
-        self.bot.run(token)
+        @self.bot.event
+        async def on_member_join(member):
+            desc = f"Un merveilleux pirates du nom de {member.mention}, nous rejoinds \n" \
+                   f"souhaiter lui la bienvenue avec un bon verre de rhum  \n\n" \
+                   f"N'oubliez pas les règles suivantes : \n" \
+                   f"1) Mettez un shield 24h avant d'aller dormir \n" \
+                   f"2) Vérifiez les Check in tout les jours \n" \
+                   f"3) Ne pas attaquez les batiments sous aucun prétexte sous peine d'exclusion \n"
+
+            mbed = discord.Embed(
+                colour=(discord.Colour.dark_blue()),
+                title=f"Bienvenue chez LSB",
+                description=desc
+            )
+
+            mbed.set_image(url="https://tel.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2FTEL.2F2019.2F09.2F20.2F4ef6192a-9f74-4f2b-a4ac-96b8cc01bc72.2Ejpeg/619x450/quality/65/pirates-des-caraibes-1-dans-la-saga-disney-tous-les-ingredients-d-un-film-de-pirates-sont-reunis-ici-le-rhum.jpg")
+            await self.bot.welcom_channel.send(embed=mbed)
+            await member.send(embed=mbed)
+
+        @self.bot.event
+        async def on_member_remove(member):
+            mbed = discord.Embed(
+                colour=(discord.Colour.dark_blue()),
+                title=f"LSB vous dit Aurevoir !",
+                description=f"Nous vous remercions d'avoir participé dans notre alliance {member.mention} \n"
+            )
+            mbed.set_image(url="https://static1.terrafemina.com/articles/9/28/00/69/@/316508-anne-dieuleveut-622x0-2.jpg")
+            await self.bot.goodbye_channel.send(embed=mbed)
+
+        @self.bot.event
+        async def on_reminder(*text):
+            textes = ""
+            for t in text:
+                textes += t
+
+            desc = "@eveyone \n\n"
+            desc += "Hello tout le monde je suis SharkyBot, je vous envoie ce message pour rappeller ceci : \n\n"
+            desc += textes
+
+            mbed = discord.Embed(
+                colour=(discord.Colour.dark_blue()),
+                title=f"Sharkybot Rappel ",
+                description=desc
+            )
+            await self.bot.annonce_channel.send(embed=mbed)
+
+        # ////////////////////////// < COMMAND FUNCTION > /////////////////////////////////////////
+
+        @self.bot.command()
+        async def au_goulag_timer(ctx, username=None, _time=None):
+            await ctx.channel.purge(limit=1)
+
+            if self.bot.is_admin(ctx):
+                if _time is not None and username is not None:
+                    try:
+                        time = int(_time)
+                        self.bot.loop.create_task(au_goulag(ctx, username, True))
+                        await asyncio.sleep(time)
+                        self.bot.loop.create_task(remove_goulag(ctx, username, True))
+                    except ValueError:
+                        await ctx.channel.send(
+                            "La commande n'est pas correcte utiliser $au_goulag_timer username seconds ")
+                    finally:
+                        pass
+                else:
+                    await ctx.channel.send("La commande n'est pas correcte utiliser $au_goulag_timer username seconds ")
+            else:
+                await ctx.channel.send(f"SharkyBot dis que {ctx.author.mention} n'est pas authorisé a faire sa !")
+
+        @self.bot.command()
+        async def au_goulag(ctx, username=None, Remove=None):
+
+            if Remove is None:
+                await ctx.channel.purge(limit=1)
+
+            if self.bot.is_admin(ctx):
+                if username is not None:
+                    member = self.bot.get_member(username)
+                    role = self.bot.get_role("goulag")
+                    role_lsb = self.bot.get_role("lsb member")
+                    id = self.bot.get_channel_id("au-goulag")
+                    channel = self.bot.get_channel(id)
+                    await member.add_roles(role)
+                    await member.remove_roles(role_lsb)
+                    await channel.send(f"L'utilisateur {member.mention} à été envoyé au goulag @everyone ")
+            else:
+                await ctx.channel.send(f"SharkyBot dis que {ctx.author.mention} n'est pas authorisé à faire sa !")
+
+        @self.bot.command()
+        async def remove_goulag(ctx, username=None, Remove=None):
+
+            if Remove is None:
+                await ctx.channel.purge(limit=1)
+
+            if self.bot.is_admin(ctx):
+                if username is not None:
+                    member = self.bot.get_member(username)
+                    role = self.bot.get_role("goulag")
+                    role_lsb = self.bot.get_role("lsb member")
+                    id = self.bot.get_channel_id("au-goulag")
+                    channel = self.bot.get_channel(id)
+                    await member.remove_roles(role)
+                    await member.add_roles(role_lsb)
+                    await channel.send(f"L'utilisateur {member.mention} à été sortie du goulag @everyone ")
+            else:
+                await ctx.channel.send(f"SharkyBot dis que {ctx.author.mention} n'est pas authorisé a faire sa !")
+
+        @self.bot.command()
+        async def hello(ctx):
+            await ctx.channel.purge(limit=1)
+            await ctx.channel.send("Bonjour " + str(ctx.author.mention) + " !")
+
+        @self.bot.command()
+        async def get_members(ctx):
+            await ctx.channel.purge(limit=1)
+            members = ""
+            if self.bot.is_admin(ctx):
+                for guild in self.bot.guilds:
+                    for m in guild.members:
+                        members += m.name + " - "
+                await ctx.channel.send("members : " + members)
+            else:
+                await ctx.channel.send(f"SharkyBot dis que {ctx.author.mention} n'est pas authorisé a faire sa !")
+
+        @self.bot.command()
+        async def ping(ctx, arg=None):
+            await ctx.channel.purge(limit=1)
+
+            if self.bot.is_admin(ctx):
+                if arg == "pong":
+                    await ctx.channel.send("Vous avez déjà Ping vous-même !")
+                else:
+                    await ctx.channel.send(str(ctx.author.mention) + " Pong !")
+            else:
+                await ctx.channel.send(f"SharkyBot dis que {ctx.author.mention} n'est pas authorisé a faire sa !")
+
+        # ////////////////////////// < CLEAR FUNCTION > /////////////////////////////////////////
+
+        @self.bot.command()
+        async def clear_limit(ctx, arg):
+            await ctx.channel.purge(limit=1)
+            if self.bot.is_admin(ctx):
+                try:
+                    nb = int(arg)
+                    await ctx.channel.purge(limit=nb)
+                except ValueError:
+                    await ctx.channel.send('commande impossible veuillez entrer un numéro ! => $clear_limit (number)')
+                finally:
+                    pass
+            else:
+                await ctx.channel.send(f"SharkyBot dis que {ctx.author.mention} n'est pas authorisé a faire sa !")
+
+        @self.bot.command()
+        async def clear_channel(ctx, arg=None):
+            await ctx.channel.purge(limit=1)
+
+            if self.bot.is_admin(ctx):
+                if arg is None:
+                    ctx.channel.send('Veuillez entrer un channel ! Sinon je ne peut pas le nettoyer :)')
+                else:
+                    try:
+                        id = self.bot.get_channel_id(arg)
+                        channel = self.bot.get_channel(id)
+                        if type(channel) is not None:
+                            await channel.purge()
+                        else:
+                            ctx.channel.send('Channel introuvable veuillez entrer le bon channel !')
+                    except ValueError:
+                        ctx.channel.send('Channel introuvable veuillez entrer le bon channel !')
+                    finally:
+                        pass
+            else:
+                await ctx.channel.send(f"SharkyBot dis que {ctx.author.mention} n'est pas authorisé a faire sa !")
+
+        @self.bot.command()
+        async def clear_all(ctx, channel=None):
+            await self.bot.clear_all(ctx, channel)
+
+        # ////////////////////////// < TIMER FUNCTION > /////////////////////////////////////////
+
+        @self.bot.command(name="remind")
+        async def remind(ctx, time, *, text):
+            await ctx.channel.purge(limit=1)
+
+            if self.bot.is_admin(ctx):
+                """Remind to do something on a date.
+                The date must be in ``Y/M/D`` format."""
+                date = datetime(*map(int, time.split("/")))
+                self.bot.timer_manager.create_timer("reminder", date, args=text)
+            else:
+                await ctx.channel.send(f"SharkyBot dis que {ctx.author.mention} n'est pas authorisé a faire sa !")
+
+        @self.bot.command()
+        async def new_annonce(ctx, timers, *_desc):
+            await ctx.channel.purge(limit=1)
+
+            if self.bot.is_admin(ctx):
+                try:
+                    desc = ""
+                    for d in _desc:
+                        desc += d
+
+                    print(str(timers) + " " + str(desc))
+                    self.bot.loop.create_task(self.bot.global_annonce(int(timers), str(desc)))
+                except ValueError:
+                    await ctx.channel.send(
+                        "Commande non acceptée timer n'est pas un chiffre en secondes ! => $new_annnonce 120 annonce")
+            else:
+                await ctx.channel.send(f"SharkyBot dis que {ctx.author.mention} n'est pas authorisé a faire sa !")
+
+        # ////////////////////////// < EVENTS FUNCTION > /////////////////////////////////////////
+
+        # @self.bot.command()
+        # async def next_event(ctx, current_event=None):
+        #     await ctx.channel.purge(limit=1)
+        #
+        #     if current_event is not None:
+        #         desc = "events    -    date de début \n\n"
+        #         data = self.predict_events(current_event)
+        #         total = len(data.events)
+        #         index = 0
+        #
+        #         while index < total:
+        #             desc += data.events[index] + "    " + data.dates[index] + "\n\n"
+        #             index += 1
+        #
+        #         mbed = discord.Embed(
+        #             colour=(discord.Colour.dark_blue()),
+        #             title=f"Sharkybot : voici la liste des prochains events ",
+        #             description=desc
+        #         )
+        #         await ctx.channel.send(embed=mbed)
+        #     else:
+        #         await ctx.channel.send(
+        #             "Si vous voulez savoir quel est le prochains events donnez moi l'évènement en cours \n"
+        #             "obtenez la liste des évents ICI $list_event ou $next_event BATIMENT")
+
+        # @self.bot.command()
+        # async def list_event(ctx):
+        #     await ctx.channel.purge(limit=1)
+        #     desc = ""
+        #     data = predict_events("BATIMENT")
+        #     for d in data.event:
+        #         desc += d + " - "
+        #
+        #     mbed = discord.Embed(
+        #         colour=(discord.Colour.dark_blue()),
+        #         title=f"Sharkybot : prédiction des évènements suivants : ",
+        #         description=desc
+        #     )
+        #     await ctx.channel.send(embed=mbed)
+
+    def running_bot(self):
+        self.bot.run(self.bot.token)
 
 
 if __name__ == '__main__':
     bot = SharkyBot()
-    bot.running_bot('')
+    bot.running_bot()
--- SharkyBot/ASharkyBot.py
+++ SharkyBot/ASharkyBot.py
@@ -1,14 +1,42 @@
+import asyncio
 
 from discord.ext import commands
 import re
 from datetime import datetime
+import os
+from configparser import ConfigParser
+import discord
 
 
 class ASharkyBot(commands.Bot):
 
     def __init__(self, command_prefix, **options):
+        self.goodbye_channel = None
+        self.goodbye_channel_img = None
+        self.welcom_channel = None
+        self.welcom_channel_img = None
+        self.annonce_channel = None
+        self.abyss_day = None
+        self.token = None
+        self.init()
         super().__init__(command_prefix, **options)
-        self.abyss_day = 26
+
+    # ////////////////////////// < INIT FUNCTION > /////////////////////////////////////////
+
+    def init(self):
+        if os.path.isfile('data/config.ini'):
+            config = ConfigParser()
+            config.read('data/config.ini')
+            self.welcom_channel = self.get_channel(self.get_channel_id(config['BOTDATA']['WELCOME_CHANNEL']))
+            self.goodbye_channel = self.get_channel(self.get_channel_id(config['BOTDATA']['GOODBYE_CHANNEL']))
+            self.annonce_channel = self.get_channel(self.get_channel_id(config['BOTDATA']['CHANNEL_ANNONCE']))
+            self.welcom_channel_img = config['BOTDATA']['WELCOME_CHANNEL_IMG']
+            self.goodbye_channel_img = config['BOTDATA']['GOODBYE_CHANNEL_IMG']
+            self.command_prefix = config['BOTDATA']['PREFIX']
+            self.abyss_day = config['BOTDATA']['ABYSS_DAY']
+            self.token = config['BOTDATA']['TOKEN']
+        else:
+            print("File not found : config.ini ")
 
     # ////////////////////////// < HELPERS FUNCTION > /////////////////////////////////////////
 
@@ -56,4 +84,71 @@ class ASharkyBot(commands.Bot):
         perm = ctx.author.guild_permissions
         return perm.administrator
 
+    async def clear_all(self, ctx, channel=None):
+        if channel is None:
+            await ctx.channel.purge(limit=1)
 
+            if self.is_admin(ctx):
+                await ctx.channel.purge()
+            else:
+                await ctx.channel.send(f"SharkyBot dis que {ctx.author.mention} n'est pas authorisé a faire sa !")
+        else:
+            await channel.purge()
+
+    # ////////////////////////// < ANNONCES FUNCTION > /////////////////////////////////////////
+
+    async def annonce(self, message):
+        desc = "@eveyone \n\n"
+        desc += "Hello tout le monde je suis SharkyBot, je vous envoie ce message pour rappeller ceci : \n\n"
+        desc += message
+
+        mbed = discord.Embed(
+            colour=(discord.Colour.dark_blue()),
+            title=f"Sharkybot Annonce",
+            description=desc
+        )
+
+        await self.annonce_channel.send(embed=mbed)
+
+    async def global_annonce(self, _timers, _desc):
+        while True:
+            desc = "@eveyone \n\n"
+            desc += "Hello tout le monde je suis SharkyBot, je vous envoie ce message pour rappeller ceci : \n\n"
+            desc += _desc
+
+            mbed = discord.Embed(
+                colour=(discord.Colour.dark_blue()),
+                title=f"Sharkybot Rappel ",
+                description=desc
+            )
+
+            await self.annonce_channel.send(embed=mbed)
+            await asyncio.sleep(_timers)
+
+    async def annonce_quotidienne(self, hours_global, _desc):
+        annonce = False
+        first_day = datetime.now().day
+
+        while True:
+            hours = datetime.now().hour
+            day = datetime.now().day
+
+            if day != first_day:
+                first_day = day
+                annonce = False
+
+            if hours == hours_global and not annonce:
+                desc = "@eveyone \n\n"
+                desc += "Hello tout le monde je suis SharkyBot,\n Je vous rappelle ceci : \n\n"
+                desc += _desc
+
+                mbed = discord.Embed(
+                    colour=(discord.Colour.dark_blue()),
+                    title=f"Sharkybot Rappel ",
+                    description=desc
+                )
+
+                await self.clear_all(None, channel=self.annonce_channel)
+                await self.annonce_channel.send(embed=mbed)
+                annonce = True
+            await asyncio.sleep(300)
